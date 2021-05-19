a90115d61b092dc22fff29eb02a0ad57af819a65
Saurabh Kumar <43825814+saurabhkumar23@users.noreply.github.com>
2021-05-19 12:44:46 +0530

    ask for modify operation option

--- main.js
+++ main.js
@@ -28,3 +28,52 @@ if(operation !=  '1' && operation != '2'){
     console.log('Sorry, we have only these operations available. choose correct option :)')
     process.exit()
 }
+else if(operation == '1'){
+    newPlaylist = readline.question('Give your playlist name: ')
+    noOfSongsToAdd = readline.question('How many songs you want to add: ')
+    console.log('------------------------------------')
+    for(let i=0;i<noOfSongsToAdd;i++){
+        songsToAdd[i] = readline.question(`${i+1}. `)
+    }
+    main();
+}
+else{
+    modifyPlaylist = readline.question('Give your playlist name: ')
+    console.log('------------------------------------')
+    modifyOperation = readline.question('Choose your operation:\n1. Add new songs. \n2. Remove songs. \n3. Add and Remove both.\n')
+    console.log('------------------------------------')
+    if(modifyOperation !=  '1' && modifyOperation != '2' && modifyOperation != '3'){
+        console.log('Sorry, we have only these operations available. choose correct option :)')
+        process.exit()
+    }
+    else if(modifyOperation == '1'){
+        noOfSongsToAdd = readline.question('How many songs you want to add: ')
+        console.log('------------------------------------')
+        for(let i=0;i<noOfSongsToAdd;i++){
+            songsToAdd[i] = readline.question(`${i+1}. `)
+        }
+        main();
+    }
+    else if(modifyOperation == '2'){
+        noOfSongsToRemove = readline.question('How many songs you want to remove: ')
+        console.log('------------------------------------')
+        for(let i=0;i<noOfSongsToRemove;i++){
+            songsToRemove[i] = readline.question(`${i+1}. `)
+        }
+        main();
+    }
+    else{
+        noOfSongsToAdd = readline.question('How many songs you want to add: ')
+        console.log('------------------------------------')
+        for(let i=0;i<noOfSongsToAdd;i++){
+            songsToAdd[i] = readline.question(`${i+1}. `)
+        }
+        console.log('------------------------------------')
+        noOfSongsToRemove = readline.question('How many songs you want to remove: ')
+        console.log('------------------------------------')
+        for(let i=0;i<noOfSongsToRemove;i++){
+            songsToRemove[i] = readline.question(`${i+1}. `)
+        }
+        main();
+    }
+}
